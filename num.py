393dbe4604158d29a24a27f0ac64dd3733e3a2a5
vinothkumarbv <37235499+vinothkumarbv@users.noreply.github.com>
2018-03-10 12:42:58 +0530

    Add files via upload

---
+++ num.py
@@ -0,0 +1,32 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      RGCET
+#
+# Created:     10/03/2018
+# Copyright:   (c) RGCET 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+a=10
+b=20
+c=25
+d=40
+e=30
+f=32
+g=35
+h=41
+i=2
+j=5
+print (a+b+c)
+print (d-e+f)
+print (h/i)
+print (i*j)
+print (b%i)
+print (j**i)
+print (a//b)
+if __name__ == '__main__':
+    main()
